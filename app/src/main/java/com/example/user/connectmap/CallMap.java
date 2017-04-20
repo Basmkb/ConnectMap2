af00dcd4d72078c540c1c820420d52bae90ca0a4
Basmkb <basgenz11@gmail.com>
2017-04-20 15:07:13 +0700

    Map Complete

--- app/src/main/java/com/example/user/connectmap/CallMap.java
+++ app/src/main/java/com/example/user/connectmap/CallMap.java
@@ -1,7 +1,11 @@
 package com.example.user.connectmap;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CallMap extends AppCompatActivity {
 
@@ -9,7 +13,22 @@ public class CallMap extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_map);
-
     } //onCreate Method
+    public  void callMap (View view){
+        // Initial view
+        EditText latitude = (EditText) findViewById(R.id.mapLat);
+        EditText longtitude = (EditText) findViewById(R.id.mapLong);
+        EditText label = (EditText) findViewById(R.id.mapLabel);
+
+        String lat = latitude.getText().toString().trim();
+        String lng = longtitude.getText().toString().trim();
+        String mlabel = label.getText().toString().trim();
+
+        Uri location = Uri.parse("https://map.google.com/maps?z=10&q=loc:"+lat+","+lng+"("+mlabel+")");
+        Intent mapIntent =new Intent(Intent.ACTION_VIEW,location);
+        //mapIntent.setPackage("com.google.android.apps.maps"); //ไม่สามารถใช้กับจินนี่โมชันได้
+        startActivity(mapIntent);
+
+    }//call Map
 
 } //Main Class
